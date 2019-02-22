f93a42ba1b01d4182c690439b2e51c779e761db0
Ayush Likhar <neontechhead@gmail.com>
2019-02-22 07:39:34 +0530

    users link added

---
+++ resources/js/views/UsersIndex.vue
@@ -0,0 +1,79 @@
+import UsersIndex from './views/UsersIndex';
+
+const router = new VueRouter({
+    mode: 'history',
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: Home
+        },
+        {
+            path: '/hello',
+            name: 'hello',
+            component: Hello,
+        },
+        {
+            path: '/users',
+            name: 'users.index',
+            component: UsersIndex,
+        },
+    ],
+});
+The UsersIndex Component
+The router defines a route using the UsersIndex component; here’s what the file (located at resources/assets/js/views/UsersIndex.vue) looks like:
+
+<template>
+    <div class="users">
+        <div class="loading" v-if="loading">
+            Loading...
+        </div>
+
+        <div v-if="error" class="error">
+            <p>{{ error }}</p>
+
+            <p>
+                <button @click.prevent="fetchData">
+                    Try Again
+                </button>
+            </p>
+        </div>
+
+        <ul v-if="users">
+            <li v-for="{ name, email } in users">
+                <strong>Name:</strong> {{ name }},
+                <strong>Email:</strong> {{ email }}
+            </li>
+        </ul>
+    </div>
+</template>
+<script>
+import axios from 'axios';
+export default {
+    data() {
+        return {
+            loading: false,
+            users: null,
+            error: null,
+        };
+    },
+    created() {
+        this.fetchData();
+    },
+    methods: {
+        fetchData() {
+            this.error = this.users = null;
+            this.loading = true;
+            axios
+                .get('/api/users')
+                .then(response => {
+                    this.loading = false;
+                    this.users = response.data;
+                }).catch(error => {
+                    this.loading = false;
+                    this.error = error.response.data.message || error.message;
+                });
+        }
+    }
+}
+</script>
